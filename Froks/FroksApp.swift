3f9ed0374d56dca8fd4d203d2de4e3d7ffa76b10
Alex Vasilyev <olorin2007@gmail.com>
2022-08-04 16:21:29 +0200

    Initial Commit

---
+++ Froks/FroksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FroksApp.swift
+//  Froks
+//
+//  Created by Alex Vasyliev on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct FroksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
